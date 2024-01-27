09a836a006b9c3f589396e81573ed20f22a17585
Ricardo <ricardo.hernandez@babelgroup.com>
2024-01-26 20:47:55 -0600

    ADDED Project with material ui

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        hello world
+      </Button>
+    </div>
+  );
+}
+
+export default App;
